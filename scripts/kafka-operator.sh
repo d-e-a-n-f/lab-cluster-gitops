7b396381879b8876301630a758c05e7107d0af57
Dean Foran <dean.foran@kes.global>
2022-08-30 14:39:47 +0100

    fix script

--- scripts/kafka-operator.sh
+++ scripts/kafka-operator.sh
@@ -3,7 +3,7 @@
 # ArgoCD Create Kafka Backbone Operator
 argocd app create kafka-operator \
 --repo https://github.com/d-e-a-n-f/lab-cluster-gitops.git \
---path infra/kafka \
+--path operators/kafka \
 --dest-namespace kafka \
 --dest-server https://kubernetes.default.svc \
 --directory-recurse \
