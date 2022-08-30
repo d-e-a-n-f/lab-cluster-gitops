5d57a461e67dbecf19b5827cccafa89095c794ea
Dean Foran <dean.foran@kes.global>
2022-08-30 14:43:25 +0100

    add cluster

---
+++ scripts/kafka-lab-cluster.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# ArgoCD Create Kafka Backbone Operator
+argocd app create kafka-lab-cluster \
+--repo https://github.com/d-e-a-n-f/lab-cluster-gitops.git \
+--path infra/kafka \
+--dest-namespace kafka \
+--dest-server https://kubernetes.default.svc \
+--directory-recurse \
+--insecure
