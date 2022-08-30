fd37bfe92fa6ad7024ab15271846b4502f52c486
Dean Foran <dean.foran@kes.global>
2022-08-30 13:15:06 +0100

    clenaup

--- scripts/pg-operators.sh
+++ scripts/pg-operators.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # ArgoCD Create Postgres Operator
-argocd app create postgres-operator --repo https://github.com/d-e-a-n-f/lab-cluster-gitops.git --path operators/postgres-operator/charts --dest-namespace default --dest-server https://kubernetes.default.svc --directory-recurse
+argocd app create postgres-operator --repo https://github.com/d-e-a-n-f/lab-cluster-gitops.git --path operators/postgres-operator/manifests/ --dest-namespace default --dest-server https://kubernetes.default.svc --directory-recurse --insecure
