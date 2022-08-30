f05269df88882613e5e7bbd9a9f99e0c1c153280
Dean Foran <dean.foran@kes.global>
2022-08-30 13:37:13 +0100

    add keycloak

---
+++ scripts/keycloak-app.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# ArgoCD Create Keycloak App (Identity Provider)
+argocd app create keycloak \
+--repo https://github.com/d-e-a-n-f/lab-cluster-gitops.git \
+--path infra/keycloak \
+--dest-namespace keycloak \
+--dest-server https://kubernetes.default.svc \
+--directory-recurse \
+--insecure
