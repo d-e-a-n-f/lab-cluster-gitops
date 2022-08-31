67ec552b0ed23bdd59a692530fd3c2d5f6696600
Dean Foran <dean.foran@kbx.com>
2022-08-31 19:13:17 +0100

    fix secret name

--- scripts/keycloak-app.sh
+++ scripts/keycloak-app.sh
@@ -4,7 +4,7 @@
 argocd app create keycloak \
 --repo https://github.com/d-e-a-n-f/lab-cluster-gitops.git \
 --path infra/keycloak \
---dest-namespace keycloak \
+--dest-namespace default \
 --dest-server https://kubernetes.default.svc \
 --directory-recurse \
 --insecure
